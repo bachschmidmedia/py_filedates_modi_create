efafaabdb21fee73700e92cdf3e65bd2a4351b87
Baff <bachschmid.media@gmail.com>
2020-07-02 22:42:04 +0200

    Added full functional GUI

--- run.py
+++ run.py
@@ -12,14 +12,17 @@ date = datetime.datetime.now()
 date_str = date.strftime('%m/%d/%Y %H:%M:%S')
 modTime = time.mktime(date.timetuple())
 user_os = platform.system()
-list = []
+my_list = []
+l_count = 0
+
 
 # Set Modification-Datetime to File
-def setModificationTime(filepath):
+def set_modification_time(filepath):
     os.utime(filepath, (modTime, modTime))
 
+
 # Set Creation-Datetime to File
-def setCreationTime(filepath):
+def set_creation_time(filepath):
     # format filepath
     new_filepath = '"{}"'.format(filepath.replace('\"', '\\"'))
 
@@ -30,50 +33,112 @@ def setCreationTime(filepath):
         # Set on Mac
         os.system('SetFile -d "{}" {}'.format(date_str, new_filepath))
 
-def set_list():
-    for f in sorted(glob.iglob('**/*', recursive=True)):
+
+def set_list(start='', recursive=True):
+    global my_list
+    my_list = []
+
+    if recursive:
+        basepath = '{}/**/*'
+    else:
+        basepath = '{}/*'
+
+    for f in sorted(glob.iglob(basepath.format(start), recursive=True)):
         if f.startswith(('venv', '.git')) == False and f != script:
             fpath = os.path.realpath(f)
-            list.append(fpath)
+            if os.path.isfile(fpath):
+                my_list.append(fpath)
+
+
+def get_list():
+    return my_list
+
 
 # Set The Dates
-def set_dates():
+def set_file_dates():
+    res = []
+    for f in get_list():
+        set_modification_time(f)
+        set_creation_time(f)
+        res.append('Datum verändert für: {}'.format(f))
+    return res
 
-    setModificationTime(fpath)
-    setCreationTime(fpath)
-
-    print('Set the Date on: {}'.format(fpath))
 
 # Main Process
 def runMain():
-    # Set List
-    set_list()
+    s_update_files = 'Dateien (Datum) anpassen'
+    global l_count
 
     # SG Theme
-    sg.theme('DarkAmber')  # Add a touch of color
+    sg.theme('LightBlue2')  # Add a touch of color
+
 
     # All the stuff inside your window.
+
     layout = [
-        [sg.Text('Aenderungs- und Erstelldatum anpassen')],
-        [sg.Button('Dateien suchen'), sg.Button('Cancel')],
-        [sg.InputText('Wähle den Ordner'), sg.FolderBrowse('Ordner auswählen')]
+        [sg.Text('Änderungsdatum und Erstelldatum von Dateien anpassen')],
+        [sg.Text('Bitte wählen Sie den Ordner aus:')],
+        [sg.Checkbox('Unterordner mit einbeziehen?', key='recursive')],
+        [sg.FolderBrowse('Ordner auswählen', key='folder_browser'), sg.InputText()],
+        [sg.Submit('Scan starten', key='scan'), sg.Submit(s_update_files, key='try_update_files', disabled=True)],
+        [sg.Listbox(values=get_list(), bind_return_key=False, size=(100, 10), key='list_box')],
+        [sg.MLine(key='subinfo')],
+        [sg.Cancel('Schließen', key='close')]
     ]
 
     # Create the Window
-    window = sg.Window('Window Title', layout)
+    window = sg.Window('Datei-Daten anpassen', layout).Finalize()
 
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
+        close_app = False
         event, values = window.read()
-        if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+
+        # On Scanning
+        if event == 'scan':
+            print('Scan! Event:', event)
+
+            path = values['folder_browser']
+            set_list(path, recursive=values['recursive'])
+
+            l_count = len(get_list())
+
+            window.FindElement('list_box').Update(
+                values=get_list()
+            )
+            window.FindElement('try_update_files').Update(
+                disabled=l_count <= 0,
+                text="{} {}".format(l_count, s_update_files)
+            )
+            window.FindElement('subinfo').Update(
+                'Insgesamt "{}" Dateien wurden gefunden!'.format(l_count)
+            )
+            window.Finalize()
+
+        # On Modify Dates
+        if event == 'try_update_files':
+            update_files = sg.PopupYesNo(
+                'Dateien anpassen?', 'Möchten Sie die Dateien anpassen?',
+                line_width=200,
+                keep_on_top=True
+            )
+
+            if update_files == 'Yes':
+                changes = set_file_dates()
+
+                succ_text = '\r\nERFOLG!!!\r\n\r\n {} Dateien erfolgreich verändert!\r\n\r\n'.format(l_count)
+                succ_changes = '\r\n => '.join(changes)
+
+                close_app = sg.PopupScrolled(succ_text + succ_changes + succ_text)
+
+        if event == sg.WIN_CLOSED or event == 'close' or close_app:  # if user closes window or clicks cancel
             break
-        print('You entered ', values[0])
 
     window.close()
 
     # print('main')
-    # set_dates()
     # print('second')
+
 
 if __name__ == "__main__":
     runMain()
